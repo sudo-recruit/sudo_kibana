8e67fcc5a74c903b18e15a53767657bac0324529
ocowchun <ocowchun@gmail.com>
2016-04-19 16:39:13 +0800

    adjsut metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
 name "sudo_kibana"
-version "0.1.6"
+version "0.0.1"
 maintainer "ocowchun"
 maintainer_email "ben.yeh@sudo.com.tw"
 license  "MIT"
 supports "ubuntu"
 description "install kibana"
-# long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 
 depends "kibana_lwrp", "~> 3.0.2"
 
